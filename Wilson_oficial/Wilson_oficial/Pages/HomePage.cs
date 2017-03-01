adb45cd4cc0db647df8e7f54425be2c30e746127
Wilson Limeira <wilson.limeira@gmail.com>
2017-03-01 19:32:26 +0000

    Comit Oficial
    - Depois de varios erros...

---
+++ Wilson_oficial/Wilson_oficial/Pages/HomePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Wilson_oficial.Pages
+{
+	public class HomePage : TabbedPage
+	{
+		public HomePage ()
+		{
+            Title = "Apollo";
+
+            //TODO Verificar se a pagina nao foi aberta antes
+            Children.Add(new SearchPage()
+            {
+                Title = "Search"
+                //TODO Add icon
+            });
+		}
+	}
+}
---
+++ Wilson_oficial/Wilson_oficial/Pages/SearchPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Wilson_oficial.Pages
+{
+	public class SearchPage : ContentPage
+	{
+		public SearchPage ()
+		{
+			
+		}
+	}
+}

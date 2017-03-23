71ada55f319a5a69d3117d6447bf7de2f55fc7f0
Wilson Limeira <wilson.limeira@gmail.com>
2017-03-23 02:02:51 +0000

    Adding File Reading
    - The app will read the file from the project to generate the first
    dictionary list
    - Those files are in "txt" already

--- Wilson_oficial/Wilson_oficial/Pages/SearchPage.xaml.cs
+++ Wilson_oficial/Wilson_oficial/Pages/SearchPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PCLStorage;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +14,7 @@ namespace Wilson_oficial.Pages
 	public partial class SearchPage : ContentPage
 	{
         private List<Word> _words;
+        private IFile file;
 
 		public SearchPage ()
 		{
@@ -33,10 +37,13 @@ namespace Wilson_oficial.Pages
 
         public void ShowWordSection(Word item)
         {
-            var title = PCLHelper.ReadAllTextAsync("dict.txt").Result;
+            //WORKING var textFromFile = await file.ReadAllTextAsync();
+
+            var textFromFile = ReadDictFiles.read();
+
             var text = new Label()
             {
-                Text = title,
+                Text = textFromFile,
                 FontSize = 18
             };
 
@@ -50,15 +57,14 @@ namespace Wilson_oficial.Pages
             //TODO IMPLEMENT HERE
         }
 
-        private void List_words_ItemTapped(object sender, ItemTappedEventArgs e)
+        private async void List_words_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             list_words.IsVisible = false; //hide the list of the View
 
             var item = e.Item as Word;
 
             //Testing how PCL works now:
-            var file = PCLHelper.CreateFile("dict.txt").Result;
-            var boola = PCLHelper.WriteTextAllAsync(file.Name, "wilson eh safadao").Result;
+            await CreateRealFileAsync();
 
             ShowWordSection(item);
 
@@ -69,6 +75,22 @@ namespace Wilson_oficial.Pages
             //TODO save searched word http://stackoverflow.com/questions/31655327/how-can-i-save-some-user-data-locally-on-my-xamarin-forms-app
             
             
+        }
+
+        public async Task CreateRealFileAsync()
+        {
+            // get hold of the file system
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+
+            // create a folder, if one does not exist already
+            IFolder folder = await rootFolder.CreateFolderAsync("MySubFolder", CreationCollisionOption.OpenIfExists);
+
+            // create a file, overwriting any existing file
+            //IFile file = await folder.CreateFileAsync("MyFile.txt", CreationCollisionOption.ReplaceExisting);
+            this.file = await folder.GetFileAsync("MyFile.txt");
+
+            // populate the file with some text
+            //await file.WriteAllTextAsync("Sample Text...");
         }
 
         private void Search_field_TextChanged(object sender, TextChangedEventArgs e)
---
+++ Wilson_oficial/Wilson_oficial/ReadDictFiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Wilson_oficial
+{
+    class ReadDictFiles
+    {
+        
+        public static string read()
+        {
+            #if __IOS__
+                string resourcePrefix = "Wilson_oficial.iOS.";
+            #elif __ANDROID__
+                string resourcePrefix = "Wilson_oficial.Droid.";
+            #endif
+
+            var assembly = typeof(ReadDictFiles).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(resourcePrefix + "Dictionaries.Test.txt");
+
+            return new System.IO.StreamReader(stream).ReadLine();
+        }
+    }
+}

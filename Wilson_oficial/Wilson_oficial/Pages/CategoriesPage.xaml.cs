470c9455e69b9edf38ac45bdae6179ee9366024b
Wilson Limeira <wilson.limeira@gmail.com>
2017-03-25 01:33:54 +0000

    Categories Page changed to C# Style instead of XAML
    - The searching error still continues though
    
    * Tested on Android

--- Wilson_oficial/Wilson_oficial/Pages/CategoriesPage.xaml.cs
+++ Wilson_oficial/Wilson_oficial/Pages/CategoriesPage.xaml.cs
@@ -12,6 +12,8 @@ namespace Wilson_oficial.Pages
 	{
         ApolloDictionary app;
         private List<WordDefinition> _words;
+        ListView list_words;
+        SearchBar search_field;
 
         public CategoriesPage ()
 		{
@@ -21,6 +23,35 @@ namespace Wilson_oficial.Pages
             app.List = ReadDictFiles.readAndBuildDictionary();
             _words = app.List;
 
+            //Creating ListView
+            list_words = new ListView
+            {
+                ItemTemplate = new DataTemplate(typeof(TextCell))
+                {
+                    Bindings = {
+                            { TextCell.TextProperty, new Binding ("Name") }
+                        }
+                },
+
+                GroupDisplayBinding = new Binding("Key"),
+                GroupShortNameBinding = new Binding("Key"),
+                IsGroupingEnabled = true,
+                ItemsSource = Listing(),
+            };
+
+            //Creating SearchBar
+            search_field = new SearchBar
+            {
+                Placeholder = "Search here..."
+            };
+
+            //Adding items to Content
+            Content = new StackLayout
+            {
+                Padding = new Thickness (5,5,5,5),
+                Children = { search_field, list_words }
+            };
+
             //Search when press the Search button
             search_field.SearchButtonPressed += Search_field_SearchButtonPressed;
 
@@ -28,10 +59,13 @@ namespace Wilson_oficial.Pages
             search_field.TextChanged += Search_field_TextChanged;
 
             //Add the list to the XAML
-            list_words.ItemsSource = Listing();
+            //list_words.ItemsSource = Listing();
 
             //Click on the item
             list_words.ItemTapped += List_words_ItemTapped;
+
+            
+            
         }
 
         private void Search_field_TextChanged(object sender, TextChangedEventArgs e)
@@ -40,11 +74,6 @@ namespace Wilson_oficial.Pages
             {
                 list_words.ItemsSource = Listing();
             }
-        }
-
-        private void Search_field_Unfocused(object sender, FocusEventArgs e)
-        {
-            list_words.ItemsSource = Listing();
         }
 
         private void Search_field_SearchButtonPressed(object sender, EventArgs e)
